39cd892111436cebf1f7cf87d709ba209b001ed2
floflo95520 <133199080+floflo95520@users.noreply.github.com>
2023-12-29 16:08:53 +0100

    Update projet.sh

--- projet.sh
+++ projet.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-if [ ! -f $1 ];
-then
-	echo "Erreur. Le premier argument n'est pas un fichier."
-fi
-
 nb_arguments=$#
 help=0
+
+if [ $nb_arguments -eq 0 ];
+then
+	echo "Aucun argument detecte."
+	exit 1
+fi
+
+
+
+
 for (( i=1 ; i<=nb_arguments ; i++ )) do
 if [ "${!i}" == "-h" ];
 then
@@ -15,7 +20,21 @@ fi
 done
 
 
-if [ $help -eq 1 ];
-then
+if [ $help -eq 1 ];then
 	echo "Message d'aide."
+else 
+	if [ ! -f $1 ]; then
+		echo "Erreur. Le premier argument n'est pas un fichier."
+	else 
+	for (( k=2 ; k<=nb_arguments ; k++ )) do
+		case ${!k} in
+			-d1) echo "option d1" ;;
+			-d2) echo "option d2" ;;
+			-l) echo "option l";;
+			-t) echo "option t";;
+			-s) echo "option s";;
+			*) echo "opérateur ${!k} non reconnu.";; 
+		esac
+	done
+	fi
 fi

aa012fece7905f3061786df460aa06fb00eef135
floflo95520 <133199080+floflo95520@users.noreply.github.com>
2024-01-31 22:49:39 +0100

    Update projet.sh

--- ProjetMi6ChapinGasmiRebour/projet.sh
+++ ProjetMi6ChapinGasmiRebour/projet.sh
@@ -11,25 +11,25 @@ fi
 
 
 
-
+#On vérifie avant tout si l'option -h est présente dans les arguments passés par l'utilisateur
 for (( i=1 ; i<=nb_arguments ; i++ )) do
 if [ "${!i}" == "-h" ];
 then
 	help=1
 fi
 done
-
+#Si le dossier existe alors on le vide
 if [ -e fichierstemporaires ];
 then
 	cd fichierstemporaires
-	rm * -r 2> fichiertempdéjàvide.txt
+	rm * -r 2> fichiertempdéjàvide.txt # si le dossier est déjà vide alors on redirige l'erreur standart dans un fichier texte qu'on supprime aussitôt
 	rm fichiertempdéjàvide.txt
 	cd ..
-else 
+else #si le dossier n'existe pas alors 
 	echo "Dossier 'fichierstemporaires' créé"
 	mkdir fichierstemporaires
 fi
-
+#si le dossier images n'existe pas alors on le crée
 if [ ! -e images ];
 then
 	echo "Dossier 'images' créé"
@@ -37,10 +37,10 @@ then
 fi
 
 
-
+#si la variable help=1 alors cela veut dire que le programme a détecté l'option -h et va donc afficher le message d'aide et ignorer les autres options
 if [ $help -eq 1 ];then
 	cat Aide.txt
-else 
+else #sinon on verifie qu'il n'y a pas d'erreur avec les arguments
 	if [ ! -f $1 ]; then
 		echo "Erreur. Le premier argument n'est pas un fichier ou n'existe pas."
 	else 
@@ -49,10 +49,12 @@ else
 -d1) 
 echo "Option -d1 :"
 AVANT=$(date +%s)
+#la commande awk sert à compter le nombre de trajets pour chaque conducteur en évitant de reprendre les lignes qui ont le meme routeid et le meme conducteur
 awk -F';' '!seen[$1,$6]++ { count[$6]++ } END {for (name in count) print count[name],";" name}' $1 | sort -r -n -k1,1 | head -n10 > fichierstemporaires/datad1_sorted.csv 
 APRES=$(date +%s)
 TEMPS=$(echo "$APRES - $AVANT" | bc)
 echo "Le temps de traitement est de $TEMPS seconde(s)"
+#programme pour générer le graphique
 gnuplot << EOF
 reset
 set terminal pngcairo size 800,1200 enhanced font "arial,12"
@@ -70,6 +72,7 @@ set yrange [0:250]
 set datafile separator ";"
 plot for [COL=1:1] "fichierstemporaires/datad1_sorted.csv" using COL:xticlabel(2) linecolor 2 title "Nombre de trajets par conducteur"
 EOF
+#on supprime les fichiers inutiles si besoin
 convert images/Résultats_d1tmp.png -rotate 90 images/Résultats_d1.png
 rm images/Résultats_d1tmp.png
 #rm fichierstemporaires/datad1_sorted.csv
@@ -80,10 +83,12 @@ rm images/Résultats_d1tmp.png
 -d2) 
 echo "Option -d2 :"
 AVANT=$(date +%s)
+#la commande awk compte la distance totale pour chaque conducteur
 awk -F';' '{ count[$6] += $5 } END {for (name in count) print count[name],";" name}' $1 | sort -r -n -k1,1 | head -n10 > fichierstemporaires/datad2_sorted.csv 
 APRES=$(date +%s)
 TEMPS=$(echo "$APRES - $AVANT" | bc)
 echo "Le temps de traitement est de $TEMPS seconde(s)"
+#programme pour générer le graphique
 gnuplot << EOF
 reset
 set terminal pngcairo size 800,1200 enhanced font "arial,12"
@@ -101,6 +106,7 @@ set yrange [0:160000]
 set datafile separator ";"
 plot for [COL=1:1] "fichierstemporaires/datad2_sorted.csv" using COL:xticlabel(2) linecolor 2 title "Distance pour chaque conducteur"
 EOF
+#on supprime les fichiers inutiles si besoin
 convert images/Résultats_d2tmp.png -rotate 90 images/Résultats_d2.png
 rm images/Résultats_d2tmp.png
 #rm fichierstemporaires/datad2_sorted.csv
@@ -110,10 +116,12 @@ rm images/Résultats_d2tmp.png
 -l) 
 echo "Option -l :"
 AVANT=$(date +%s)
+#la commande awk calcule la distance totale pour chaque trajet
 awk -F';' '{ count[$1] += $5 } END {for (num in count) print count[num],";" num}' $1 | sort -r -n -t';' -k1,1 | head -n10 | sort -r -n -t';' -k2,2 > fichierstemporaires/datal_sorted.csv
 APRES=$(date +%s)
 TEMPS=$(echo "$APRES - $AVANT" | bc)
 echo "Le temps de traitement est de $TEMPS seconde(s)"
+#programme pour générer le graphique
 gnuplot << EOF
 reset
 set terminal pngcairo size 800,800 enhanced font "arial,12"
@@ -129,12 +137,14 @@ set yrange [0:3000]
 set datafile separator ";"
 plot for [COL=1:1] "fichierstemporaires/datal_sorted.csv" using COL:xticlabel(2) linecolor 2 title "Longueur d'un trajet"
 EOF
+#on supprime les fichiers inutiles si besoin
 #rm fichierstemporaires/datal_sorted.csv
 #display images/Résultats_l.png
 ;;
 
 -s) 
 echo "Option -s :"
+#si le fichier qui va stocker les fichiers.o n'existe pas alors on le crée
 if [ ! -d Programme_C/Fichiers_o_S ];
 then
 	cd Programme_C
@@ -142,8 +152,11 @@ then
 	cd ..
 fi
 AVANT=$(date +%s)
+#à partir du fichier passé en argument, on garde uniquement les colonnes qui nous interressent tout en changeant le séparateur ce qui facilitera la lecture des données pour le programme c
 cut -d';' -f1,5 $1 | sed 's/;/ /g' | tail -n +2 > fichierstemporaires/data_s.txt
+#on récupère le nombre de lignes grâce à la variable t en retirant le nom du fichier qui s'affiche à côté
 t=`wc -l fichierstemporaires/data_s.txt | cut -d' ' -f1`
+#si l'exécutable ProgS n'est pas présent alors on le crée
 if [ ! -e Programme_C/ProgS ];
 then
 	cd Programme_C
@@ -151,10 +164,12 @@ then
 	make clean >> make.txt
 	cd ..
 fi
+#on exécute le programme en passant la valeur de t en argument pour que le programme C la récupère
 ./Programme_C/ProgS $t
 APRES=$(date +%s)
 TEMPS=$(echo "$APRES - $AVANT" | bc)
 echo "Le temps de traitement est de $TEMPS seconde(s)"
+#programme pour générer le graphique
 gnuplot << EOF
 reset
 set terminal pngcairo size 1200,800 enhanced font "arial,12"
@@ -168,15 +183,16 @@ set datafile separator ';'
 plot 'fichierstemporaires/data_s.csv' using 0:3:5:xticlabel(2) with filledcurves linecolor 2 title 'Distance (Km)',\
 ''using 0:4 with lines linecolor 0 title 'Distance moyenne (Km)'
 EOF
+#on supprime les fichiers inutiles si besoin
 rm fichierstemporaires/data_s.txt
 #rm fichierstemporaires/data_s.csv
-
 rm Programme_C/make.txt
 rm Programme_C/ProgS
 ;;
 			
 -t) 
 echo "Option -t :"
+#si le fichier qui va stocker les fichiers.o n'existe pas alors on le crée
 if [ ! -d Programme_C/Fichiers_o_T ];
 then
 	cd Programme_C
@@ -184,8 +200,11 @@ then
 	cd ..
 fi
 AVANT=$(date +%s)
+#à partir du fichier passé en argument, on garde uniquement les colonnes qui nous interressent tout en changeant le séparateur ce qui facilitera la lecture des données pour le programme c
 cut -d';' -f1-4 $1 | sed 's/ /-/g' | sed 's/;/ /g' | tail -n +2 > fichierstemporaires/data_t.txt
+#on récupère le nombre de lignes grâce à la variable t en retirant le nom du fichier qui s'affiche à côté
 t=`wc -l fichierstemporaires/data_t.txt | cut -d' ' -f1`
+#si l'exécutable ProgT n'est pas présent alors on le crée
 if [ ! -e Programme_C/ProgT ];
 then
 	cd Programme_C
@@ -193,11 +212,13 @@ then
 	make clean >> make.txt
 	cd ..
 fi
+#on exécute le programme en passant la valeur de t en argument pour que le programme C la récupère
 ./Programme_C/ProgT $t
 sed 's/-/ /g' fichierstemporaires/data_t_temp.csv > fichierstemporaires/data_t.csv
 APRES=$(date +%s)
 TEMPS=$(echo "$APRES - $AVANT" | bc)
 echo "Le temps de traitement est de $TEMPS seconde(s)"
+#programme pour générer le graphique
 gnuplot << EOF
 reset
 set terminal pngcairo size 800,800 enhanced font "arial,12"
@@ -214,6 +235,7 @@ set boxwidth 4
 set datafile separator ';'
 plot 'fichierstemporaires/data_t.csv' using 2:xtic(1) linecolor 2 title 'Nombre de trajets', '' using 3 linecolor 9 title 'Départs trajets'
 EOF
+#on supprime les fichiers inutiles si besoin
 rm fichierstemporaires/data_t_temp.csv
 rm fichierstemporaires/data_t.txt
 rm Programme_C/make.txt
